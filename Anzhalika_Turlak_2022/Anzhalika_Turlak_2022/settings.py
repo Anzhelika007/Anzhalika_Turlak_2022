0c48f53b7028c8490f0f50b5f30fd0fe4f32c0a7
Anzhalika Turlak <293852647@mail.ru>
2022-09-20 22:28:39 +0300

    add project

--- Anzhalika_Turlak_2022/Anzhalika_Turlak_2022/settings.py
+++ Anzhalika_Turlak_2022/Anzhalika_Turlak_2022/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'project'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'Anzhalika_Turlak_2022.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['Anzhalika_Turlak_2022/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
